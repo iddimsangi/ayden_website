832a7d16ee664d2847a3afa2011d26f4b42accf9
iddimsangi <adammsangi@gmail.com>
2019-11-11 12:02:41 +0300

    add scrollTop effect from jquery plugin

--- resources/js/script.js
+++ resources/js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 //  $("h1").click(function(){
 //    $(this).hide();
 //  });
+    /*-----------------STICKY NAVIGATIONS-------------------------*/
     $('.js--sec-about').waypoint(function(direction){
         if(direction == "down"){
             $('nav').addClass('sticky');
@@ -11,4 +12,8 @@ $(document).ready(function(){
     },{
          offset: 60 
     });
+    /*----------------SCROLL NAVIGATIONS---------------------------*/
+    $('.js-scroll-to-section-form').click(function(){
+        $('html, body').animate({scrollTop:$('.js--section-form').offset().top},1000);
+    })
 });

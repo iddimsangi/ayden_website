9c78f40421b349f60b4855aa051c21234a0a0d7f
iddimsangi <adammsangi@gmail.com>
2019-11-11 11:41:07 +0300

    add waypoint jquery plugin

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
-  $("h1").click(function(){
-    $(this).hide();
-  });
+//  $("h1").click(function(){
+//    $(this).hide();
+//  });
+    $('.js--sec-about').waypoint(function(direction){
+        if(direction == "down"){
+            $('nav').addClass('sticky');
+        }else{
+            $('nav').removeClass('sticky');
+        }
+    },{
+         offset: 60 
+    });
 });

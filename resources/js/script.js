6290a37a780a46efa1c553e9364d501955923097
iddimsangi <adammsangi@gmail.com>
2019-11-11 15:49:11 +0300

    added a responsive mobile nav icons

--- resources/js/script.js
+++ resources/js/script.js
@@ -34,4 +34,23 @@ $(document).ready(function(){
 		scrollTop: position
 	} /* speed */ );
 });
+    
+    /*----------------------------------mobi-nav---------------------*/
+    $('.js--nav-icon').click(function(){
+        var nav = $('.js--navigations-list');
+        var icon = $('.js--mob-nav');
+        nav.slideToggle(200);
+        if(icon.hasClass('ion-navicon')){
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon');
+        }else{
+            icon.addClass('ion-navicon');
+            icon.removeClass('ion-close-round');
+        }
+        
+    })
+    /*-------------------ANIMATE USING WAYPOINT-------------------*/
+//    $('.js--wp--1').waypoint(function(direction){
+//        $('.js--wp--1').addClass('animated bounceInRight');
+//    });
 });
